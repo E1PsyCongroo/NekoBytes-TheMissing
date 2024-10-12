7f40612a1ad94d4704a3053aca78902843146762
EQ1932 <2914420787@qq.com>
2024-10-12 21:43:56 +0800

    lab3 新增3_2

--- Labs/lab3/lab3_1/ex2_arrays.c
+++ Labs/lab3/lab3_1/ex2_arrays.c
@@ -3,27 +3,27 @@
 #include <stdlib.h>
 
 int main() {
-  // Creates an integer with value 5
-  // Note: int8_t is a numerical datatype that takes up 1 byte of memory
-  int8_t x = 5;
+  // 创建一个值为 3 的整数
+  // Note: int8_t是数值数据类型，占用1字节内存
+  int8_t x = 3;
 
-  // TODO: allocate an int8_t array of size 4
-  // Hint: C represents arrays as a pointer to the first element
-  // Hint: one malloc call allocates memory for the entire array
-  //       it works because the elements are stored side by side
+  // TODO: 分配一个大小为 4 的 int8_t 数组
+  // Hint: C语法将数组表示为指向第一个元素的指针
+  // Hint: 使用一次 malloc 调用为整个数组分配内存
+  //       它之所以有效，是因为元素并排存储
   ______ some_array = malloc(______);
   printf("address of the start of the array: %p\n", some_array);
 
-  // TODO: compute the address of the element at index 2 (0-indexed)
+  // TODO: 计算索引 2 处元素的地址（索引从0开始）
   ______ ptr_to_idx_2 = ____________;
   printf("address of index 2: %p\n", ptr_to_idx_2);
 
-  // TODO: store the value 10 at index 2, using ptr_to_idx_2
-  __________ = 10;
+  // TODO: 使用 ptr_to_idx_2 将值 9 存储在索引 2 处
+  __________ = 9;
 
-  // TODO: print the value at index 2
-  // Hint: this blank should be the same as the previous blank
-  //       please don't hard code 10
+  // TODO: 打印索引 2 处的值
+  // Hint: 此空白应与前一个空白相同
+  //       请不要hard code 10
   printf("value at index 2: %d\n", __________);
 
   return 0;
