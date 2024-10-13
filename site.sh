9c03f24691098e37d8be482614437f810a285485
focused_xy <elpsycongroo242@gmail.com>
2024-10-13 14:26:50 +0800

    feat: add codes to static site Co-authored-by: MayGe <152044636+M4yGem1ni@users.noreply.github.com>

---
+++ site.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+WORK_DIR=$(pwd)
+SITE_DIR="$WORK_DIR/static"
+SOURCE=("$WORK_DIR/index.html" "$WORK_DIR/Lectures")
+mkdir -p "$SITE_DIR"
+cp -r "${SOURCE[@]}" "$SITE_DIR"
+
+CODE_DIR="$WORK_DIR/Others/Codes"
+JEKLLY_TOOL_DIR="$WORK_DIR/Tools/ghpages-mini-fileserver"
+cp -r "$CODE_DIR" "$JEKLLY_TOOL_DIR/resources"
+bash "$JEKLLY_TOOL_DIR/prenodes.sh"
+cd "$JEKLLY_TOOL_DIR" && bundle exec jekyll build --baseurl "$1"
+cp -r "$JEKLLY_TOOL_DIR/_site" "$SITE_DIR"
