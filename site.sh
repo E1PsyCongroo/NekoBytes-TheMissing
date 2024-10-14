579bae061bbdf9fc5390755bdefbfa24ceb0bea3
focused_xy <elpsycongroo242@gmail.com>
2024-10-14 14:24:08 +0800

    fix: add codes per lecs

--- site.sh
+++ site.sh
@@ -7,8 +7,10 @@ cp -r "${SOURCE[@]}" "$SITE_DIR"
 
 JEKLLY_TOOL_DIR="$WORK_DIR/Tools/ghpages-mini-fileserver"
 RESOURCE="$JEKLLY_TOOL_DIR/resources"
-mkdir -p "$RESOURCE"
-find "$WORK_DIR/Lectures" -type d -name "Codes" -exec cp -r "{}" "$RESOURCE" \;
+find "$WORK_DIR/Lectures" -name "Codes" -type d -print | sed "s|$WORK_DIR/Lectures/||" | while read -r dir; do
+    mkdir -p "$RESOURCE/$(dirname "$dir")"
+    cp -r "$WORK_DIR/Lectures/$dir" "$RESOURCE/$dir"
+done
 bash "$JEKLLY_TOOL_DIR/prenodes.sh"
 cd "$JEKLLY_TOOL_DIR" && bundle exec jekyll build --baseurl "$1"
 cp -r "$JEKLLY_TOOL_DIR/_site" "$SITE_DIR"
