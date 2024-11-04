a285b49b6edf1521855f943a1cac421b5841061e
wenjing233 <2040553710@qq.com>
2024-11-04 23:06:05 +0800

    feat: 完成指引文档，完成单元测试设计
    perf: 格式化代码和文档
    
    Generate proj1

--- Projects/Proj1/test.c
+++
@@ -1,8 +0,0 @@
-#include <criterion/criterion.h>
-#include <string.h>
-
-Test(sample, test) {
-  cr_expect(strlen("Test") == 4, "Expected \"Test\" to have a length of 4.");
-  cr_expect(strlen("Hello") == 4, "This will always fail, why did I add this?");
-  cr_assert(strlen("") == 0);
-}
