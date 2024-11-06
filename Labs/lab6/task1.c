10707d3f0d8d7e6ffa63503eda9844e386651f96
rache <2746324475@qq.com>
2024-11-06 16:26:56 +0800

    init: lab6 init
    1. create lab6
    2. create lab6 solution

---
+++ Labs/lab6/task1.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+struct Course {
+    int id;
+    char *name;
+};
+
+struct Course *make_course(int id, char *name) {
+    struct Course *new_course = malloc(sizeof(struct Course));
+    new_course->id = id;
+    new_course->name = *name;
+
+    return &new_course;
+}
+int main() {
+    struct Course *cstart = make_course(0x001, "cstart");
+    printf("Welcome to CS%d: %s!\n", cstart->id, cstart->name);
+    free(cstart);
+
+    return 0;
+}
