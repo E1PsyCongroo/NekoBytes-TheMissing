3f1183d4ffa9a51f05f8a0b66d5029c7b051c0d6
LRache <2746324475@qq.com>
2024-11-08 18:48:37 +0800

    feat:增加了对makefile valgrind gdb说明的链接

--- Labs/lab6/pwd_checker/include/checker.h
+++
@@ -1,8 +0,0 @@
-#ifndef PWD_CHECKER_H
-#define PWD_CHECKER_H
-
-#include <stdbool.h>
-
-bool check_password(const char *first_name, const char *last_name, const char *password);
-
-#endif // PWD_CHECKER_H
